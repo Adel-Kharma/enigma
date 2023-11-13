6d073351409242036b0e7fe5281d7fdb0033a306
IbrahimHabra <ibrahimhabra04@gmail.com>
2023-11-13 11:01:31 +0200

    Add the numeric background Widget to the shared components

---
+++ lib/modules/NumBackground.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class NumBackground extends StatelessWidget {
+  final Widget child;
+
+  const NumBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/background.png'),
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(0.71, -0.71),
+            end: Alignment(-0.71, 0.71),
+            colors: [Color(0xEEABDCFF), Color(0xEE0396FF)],
+          ),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
